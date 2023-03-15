9ed8078a45f08da872d41cb19947d8a418c43633
Karol Galera <frontend.galera@gmail.com>
2023-03-15 20:01:59 +0100

    AllPosts component added to AllPostsPage route component

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,8 @@
-const AllPostsPage = () => {};
+import AllPosts from "../../components/posts/AllPosts";
+import { DUMMYDATA } from "../../dummyData";
+
+const AllPostsPage = () => {
+  return <AllPosts posts={DUMMYDATA} />;
+};
 
 export default AllPostsPage;
