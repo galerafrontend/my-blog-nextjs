b5cf65637e9a5c4b433a5b80c0733b52f7354c27
Karol Galera <frontend.galera@gmail.com>
2023-03-15 16:39:01 +0100

    array with dummyData added

---
+++ dummyData.js
@@ -0,0 +1,37 @@
+export const DUMMYDATA = [
+  {
+    slug: "getting-started-with-nextjs1",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJs is a React framework for production",
+    data: "2023-06-20",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJs is a React framework for production",
+    data: "2023-06-20",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "Getting Started with NextJS",
+    image:  "getting-started-nextjs.png",
+    excerpt: "NextJs is a React framework for production",
+    data: "2023-06-20",
+  },
+  {
+    slug: "getting-started-with-nextjs4",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJs is a React framework for production",
+    data: "2023-06-20",
+  },
+  {
+    slug: "getting-started-with-nextjs5",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJs is a React framework for production",
+    data: "2023-06-20",
+  },
+];
