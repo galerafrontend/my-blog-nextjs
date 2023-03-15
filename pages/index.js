543a64a5c70a14024c3728af35a64cef58cb68cf
Karol Galera <frontend.galera@gmail.com>
2023-03-15 16:46:35 +0100

    FeaturedPosts component added to pages/index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
+import FeaturedPosts from "../components/homepage/FeaturedPosts";
 import Hero from "../components/homepage/Hero";
+import { DUMMYDATA } from "../dummyData";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <FeaturedPosts posts={DUMMYDATA} />
     </>
   );
 };
