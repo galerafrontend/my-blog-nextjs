38d4f1e5b65a96b82e095e3b39bda53789b7de86
Karol Galera <frontend.galera@gmail.com>
2023-03-16 15:44:14 +0100

    getStaticProps and getStaticPaths added to PostDetailPage dynamic route component

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,33 @@
 import PostContent from "../../components/posts/postDeatils/PostContent";
+import { getPostData, getPostsFiles } from "../../helpers/postsUtil";
 
-const PostDetailPage = () => {
-  return <PostContent />;
+const PostDetailPage = ({ post }) => {
+  return <PostContent post={post} />;
+};
+
+export const getStaticProps = (context) => {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+};
+
+export const getStaticPaths = () => {
+  const postFileNames = getPostsFiles();
+
+  const slugs = postFileNames.map((fileName) => fileName.replace(/\.md$/, ""));
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
 };
 
 export default PostDetailPage;
