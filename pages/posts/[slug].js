da660f2a0a3c10ec6128b025c16d390640295bee
Karol Galera <frontend.galera@gmail.com>
2023-03-16 13:29:27 +0100

    PostContent component added to PostDeatilPage dynamic route component

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,3 +1,7 @@
-const PostDetailPage = () => {};
+import PostContent from "../../components/posts/postDeatils/PostContent";
+
+const PostDetailPage = () => {
+  return <PostContent />;
+};
 
 export default PostDetailPage;
