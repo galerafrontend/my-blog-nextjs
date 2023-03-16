ff9bf18cfb0cb71c9b7e9cb74bd4488101425379
Karol Galera <frontend.galera@gmail.com>
2023-03-16 15:45:07 +0100

    DUMMY_POSTS removed and post props added to PostContent component

--- components/posts/postDeatils/PostContent/index.js
+++ components/posts/postDeatils/PostContent/index.js
@@ -2,22 +2,13 @@ import PostHeader from "../PostHeader";
 import classes from "./styles.module.css";
 import ReactMarkdown from "react-markdown";
 
-const DUMMY_POSTS = {
-  slug: "getting-started-with-nextjs1",
-  title: "Getting Started with NextJS",
-  image: "getting-started-nextjs.png",
-  excerpt: "NextJs is a React framework for production",
-  date: "2023-06-20",
-  content: "# This is a first post",
-};
-
-const PostContent = () => {
-  const imagePath = `/images/posts/${DUMMY_POSTS.slug}/${DUMMY_POSTS.image}`;
+const PostContent = ({ post }) => {
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   return (
     <article className={classes.content}>
-      <PostHeader image={imagePath} title={DUMMY_POSTS.title} />
-      <ReactMarkdown>{DUMMY_POSTS.content}</ReactMarkdown>
+      <PostHeader image={imagePath} title={post.title} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 };
