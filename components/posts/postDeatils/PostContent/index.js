c02d4bc286dcd361789bb195f50156e34a5b61a4
Karol Galera <frontend.galera@gmail.com>
2023-03-17 15:54:12 +0100

    object with a condition that checks if the first element of a paragraph is an image, added to PostContent component

--- components/posts/postDeatils/PostContent/index.js
+++ components/posts/postDeatils/PostContent/index.js
@@ -1,14 +1,38 @@
 import PostHeader from "../PostHeader";
 import classes from "./styles.module.css";
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 
 const PostContent = ({ post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customRenderers = {
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader image={imagePath} title={post.title} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
