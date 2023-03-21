2e8a6e70027d9bcdfde2f514bd7fed1a9e33392a
Karol Galera <frontend.galera@gmail.com>
2023-03-21 15:29:56 +0100

    connection with database added to api routes/contact

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -19,6 +21,30 @@ const handler = (req, res) => {
       name,
       message,
     };
+
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://frontendgalera:o1Ri3uWxfe67YC1s@cluster0.vibpqwp.mongodb.net/my-site?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect with database" });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed" });
+      return;
+    }
+
+    client.close();
 
     res.status(201).json({
       message: "Successfully stored in database",
