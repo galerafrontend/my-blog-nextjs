68649a8198b8e09f9a777ff3a392ec14b8f622c1
Karol Galera <frontend.galera@gmail.com>
2023-03-21 16:22:32 +0100

    Notification display, added to the Contact Form component

--- components/contact/ContactForm/index.js
+++ components/contact/ContactForm/index.js
@@ -1,26 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { sendContactData } from "../../../helpers/contactUtil";
 import classes from "./styles.module.css";
+import Notification from "../../ui/Notification";
 
 const ContactForm = () => {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredName, setEnteredName] = useState("");
   const [enteredMessage, setEnteredMessage] = useState("");
+  const [status, setStatus] = useState();
+  const [requestError, setRequestError] = useState();
 
-  const sendMessageHandler = (event) => {
+  const sendMessageHandler = async (event) => {
     event.preventDefault();
 
-    fetch("/api/contact", {
-      method: "POST",
-      body: JSON.stringify({
+    setStatus("pending");
+
+    try {
+      await sendContactData({
         email: enteredEmail,
         name: enteredName,
         message: enteredMessage,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+      });
+      setStatus("success");
+    } catch (error) {
+      setRequestError(error.message);
+      setStatus("error");
+    }
   };
+
+  let notification;
+
+  if (status === "pending") {
+    notification = {
+      status: "pending",
+      title: "Sending message...",
+      message: "Your message is being sent now",
+    };
+  }
+  if (status === "success") {
+    notification = {
+      status: "success",
+      title: "Success!",
+      message: "Message sent successfully!",
+    };
+  }
+  if (status === "error") {
+    notification = {
+      status: "error",
+      title: "Error!",
+      message: requestError,
+    };
+  }
 
   return (
     <section className={classes.contact}>
@@ -62,6 +92,13 @@ const ContactForm = () => {
           <button>Send message</button>
         </div>
       </form>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
     </section>
   );
 };
