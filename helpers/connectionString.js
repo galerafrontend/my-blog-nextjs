99eda8fe3c07bc080e9517bbd5c8132f8f6a8f18
Karol Galera <frontend.galera@gmail.com>
2023-03-22 18:11:27 +0100

    typo corrected in connetionString file

--- helpers/connectionString.js
+++ helpers/connectionString.js
@@ -1,1 +1,1 @@
-export const connectionString = `mongodb+srv://${process.env.mdb_username}:${process.env.mdb_password}@${process.env.mdb_clustername}.vibpqwp.mongodb.net/${process.mdb_database}?retryWrites=true&w=majority`;
+export const connectionString = `mongodb+srv://${process.env.mdb_username}:${process.env.mdb_password}@${process.env.mdb_clustername}.vibpqwp.mongodb.net/${process.env.mdb_database}?retryWrites=true&w=majority`;
